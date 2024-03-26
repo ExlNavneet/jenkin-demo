4dce789ee8c9c28e210b0986b185b206c9ff4977
NZL10-DP1421$ <NZL10-DP1421$@NZL10-DP1421.corp.exlservice.com>
2024-03-26 15:20:01 +0530

    Committed by ExlNavneet

---
+++ toAddDdls.bat
@@ -0,0 +1,20 @@
+@echo OFF
+setlocal
+
+
+set "searchstartHash=startHash"
+FOR /F "tokens=*" %%g IN ('cd C:\Users\Navneet177863\Documents\Jenkins Demo\localRepo git rev-parse Head') do (SET insertsartHash=%%g)
+
+set "textFile=C:\Users\Navneet177863\Documents\cicd-demo\ADM\automated-versioning-manager-perpare-package\version-manager.properties"
+
+(FOR /f "usebackq tokens=1* delims==" %%i in ("%textFile%") do (
+   if "%%i" equ "%searchstartHash%" (
+      echo %searchstartHash%=%insertsartHash%
+   ) else if "%%j" neq "" (
+      echo %%i=%%j
+   ) else (
+      echo %%i
+   )
+)) > temp.tmp
+
+move /Y temp.tmp "%textFile%"
